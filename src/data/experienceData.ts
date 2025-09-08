e2efd860b0dd715003eb48c59e1d237c0bc78f12
Hiren Gajjar <144141125+HGajjar17@users.noreply.github.com>
2025-09-08 09:53:07 -0300

    Update experienceData.ts
    Try's to solve " and '

--- src/data/experienceData.ts
+++ src/data/experienceData.ts
@@ -12,11 +12,11 @@ export const jobs: Job[] = [
     dates: "Aug 2025 - Present",
     description: [
       "Delivered front-line technical support for computer setup, diagnostics, and repairs, resolving 
-hardware/software issues to restore functionality and minimize downtime.",
-
+hardware/software issues to restore functionality and minimize downtime."
+,
       "Guided customers through personalized tech solutions by assessing needs, configuring systems, and 
-recommending appropriate devices, ensuring improved user satisfaction.",
-
+recommending appropriate devices, ensuring improved user satisfaction."
+,
       "Trained customers on safe and efficient technology usage, strengthening digital confidence and 
 reducing repeat support requests."
     ]
